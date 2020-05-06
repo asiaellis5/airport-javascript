3d6d237c6b70eee7a47025ba8bc07a17a5110de6
Asia <asiaellis5@gmail.com>
2020-05-06 13:37:33 +0100

    added test to plane class to check if it lands correctly

--- jasmine-standalone-3.5.0/spec/planeSpec.js
+++ jasmine-standalone-3.5.0/spec/planeSpec.js
@@ -1,7 +1,16 @@
 describe("Plane", function () {
   let plane;
+  let airport;
 
   beforeEach(function () {
     plane = new Plane();
+    airport = jasmine.createSpyObj("airport", ["land"]);
+  });
+
+  describe("land", function () {
+    it("can land at an airport", function () {
+      airport.land(plane);
+      expect(airport.land).toHaveBeenCalledWith(plane);
+    });
   });
 });
